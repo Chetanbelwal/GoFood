af412b25bb7888967cb224d97dc873bc323de2ba
Chetan Belwal <chetan.belwal13@gmail.com>
2024-11-27 10:44:38 +0530

    Backend Done

--- server/models/reservation-model.js
+++ server/models/reservation-model.js
@@ -4,40 +4,39 @@ import validator from "validator";
 const reservationSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required: [true, 'First Name is required'],
-    minlength: [3, 'First Name should have a minimum of 3 characters'],
-    maxlength: [20, 'First Name cannot exceed 20 characters'],
+    required: true,
+    minlength: [3, "First Name should have a minimum of 3 characters"],
+    maxlength: [20, "First Name cannot exceed 20 characters"],
     trim: true,
   },
   lastName: {
     type: String,
-    required: [true, 'Last Name is required'],
-    minlength: [3, 'Last Name should have a minimum of 3 characters'],
-    maxlength: [20, 'Last Name cannot exceed 20 characters'],
+    required: true,
+    minlength: [3, "Last Name should have a minimum of 3 characters"],
+    maxlength: [20, "Last Name cannot exceed 20 characters"],
     trim: true,
   },
   email: {
     type: String,
-    required: [true, 'Email is required'],
-    validate: {
-      validator: validator.isEmail, // Using validator's isEmail function
-      message: 'Please provide a valid email address',
-    },
+    required: true,
+    validate: [validator.isEmail, "Please provide a valid email address"],
     trim: true,
     unique: true, // Ensures no duplicate emails in the database
   },
   phone: {
     type: String,
-    required: [true, 'Phone number is required'],
+    required: true,
+    minlength: [10, "Phone should be of Minimum 10 Digit "],
+    maxlength: [10, "Phone should be of Minimum 10 Digit"],
     trim: true, // Removes leading and trailing spaces
   },
   date: {
     type: Date,
-    required: [true, 'Date is required'],
+    required: true,
   },
   time: {
     type: String,
-    required: [true, 'Time is required'],
+    required: true,
     trim: true,
   },
 });
--- server/server.js
+++ server/server.js
@@ -3,14 +3,8 @@ import connectToDb from './utils/db.js'
 import {errorMiddleware} from './error/error.js'
 import reservationRoute from './routes/reservationRoute.js'
 
-// Middleware
-//app.use(cors()); // Enable Cross-Origin Resource Sharing
 
 
-// Define a simple route
-app.get('/', (req, res) => {
-  res.send('Welcome to the Express server!');
-});
 
 // Define port and start server
 const PORT = process.env.PORT || 5000;
