a7e4daeda6f30702d9838cb508e0f9954e2b8874
Chetan Belwal <chetan.belwal13@gmail.com>
2024-12-03 02:03:40 +0530

    Frontend

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,9 +1,19 @@
+import About from "../../components/About";
 import HeroSection from "../../components/HeroSection";
+import Qualities from "../../components/Qualities";
+import Menu from "../../components/Menu";
+import WhoAreWe from "../../components/WhoAreWe";
+import Team from "../../components/Team";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <About/>
+      <Qualities/>
+      <Menu/>
+      <WhoAreWe/>
+      <Team/>
     </>
   );
 };
