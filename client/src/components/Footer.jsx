4f9e5c97153ae99a7e2d5df356325e2e8fa978a9
Chetan Belwal <chetan.belwal13@gmail.com>
2024-12-06 00:01:35 +0530

    Frontend Completed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,27 +1,27 @@
 // import React from "react";
 
-// const Footer = () => {
-//   return (
-//     <footer>
-//       <div className="container">
-//         <div className="banner">
-//           <div className="left">ZEESHU</div>
-//           <div className="right">
-//             <p>Z6 Gulshan-e-Maymar, Karachi</p>
-//             <p>Open: 05:00 PM - 12:00 AM</p>
-//           </div>
-//         </div>
-//         <div className="banner">
-//           <div className="left">
-//             <p>Developed By CODEWITHZEESHU</p>
-//           </div>
-//           <div className="right">
-//             <p>All Rights Reserved By CodeWithZeeshu.</p>
-//           </div>
-//         </div>
-//       </div>
-//     </footer>
-//   );
-// };
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">GO FOOD</div>
+          <div className="right">
+            <p>Go Food, India</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Go Food</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Go Food.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-// export default Footer;
+export default Footer;
